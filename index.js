8845c62a94b1dee1914a11be95ff9a01cb53c5ed
Agustindvr <dvragustin@users.noreply.github.com>
2020-05-07 17:11:27 -0300

    created player data
    created name, attack and gold.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 class Robot{
 	id = 0;
-	hp = 2;
+	hp = 8;
 	gold = 2;
 	createdTime;
-	initialHp = 2;
+	initialHp = 8;
 	defense = 0;
 	
+	constructor(){
+		this.createdTime = this.getActualTime();
+	}
+
+	getActualTime(){
+		var d = new Date();
+		var actualTime = d.getTime();
+			return actualTime;
+	}
+
+
 	setId(id, position){
 		var newId = "";
 		if(position == "next"){
@@ -13,7 +24,7 @@ class Robot{
 		}	else if(position == "prev"){
 			newId = id - 1;
 		} else {
-			newId = id
+			newId = id;
 		}
 		this.id = newId;
 
@@ -24,7 +35,7 @@ class Robot{
 		if(position == "next"){
 			newHp = initialHp + initialHp;
 		} else if(position == "prev"){
-			newHp = initialHp / initialHp;
+			newHp = initialHp / 2;
 		}	else{
 			newHp = initialHp;
 		}
@@ -40,32 +51,108 @@ class Robot{
 		} else if(position == "prev"){
 			newGold = gold / 2;
 		} else {
-			newGold = gold
+			newGold = gold;
 		}
 		this.gold = newGold;
 	}
-	///ejemplo de defensa, cuando el id sea mayor o igual a 10, la defensa va a ser el hp divido 2.
-	setDefense(defense,position, id){
+	
+	setDefense(id){
 		var newDefense = "";
-		if(position == "next", id >= 10){
-		 	defense = hp / 2;
-		} else if(position =="prev",id >=10){
-			newDefense = hp / 2;
+		if(id >= 10){
+		 	newDefense = this.hp / 2;
 		} else{
-			newDefense = defense;
+			newDefense = 0;
 		}
-		
-		this.defense = newDefense;
-		
+		this.defense = newDefense;		
 	}
+
+	isDead(){
+		if(this.hp <= 0){
+			return true;
+		}
+		return false;
+	}
+}
+
+class Player{
+	id = 1;
+	name = "conejo";
+	attackDmg = 2;
+	gold = 0;
+
+	setName(name){
+    this.name = name;
+  }
+
+  setGold(gold){
+    this.gold = gold;
+  }
+
+  setAttack(attackDmg){
+    this.attackDmg = attack;
+  }
+
+  attack(Robot){
+    Robot.hp = Robot.hp - this.attackDmg;
+  }
 
 }
 
 var robot = new Robot();
-	robot.setHp(2, "next");
-	robot.setId(0,"next");
-	robot.setGold(2,"next");
-	robot.setDefense(0,"next")
-console.log(robot);
+var player = new Player();
 
-//set: gold (next,prev, =), id (next, prev, =)
+var intervalMiliSeconds = 2000;
+	setInterval(() => { 
+		updateRobotData(robot);
+		playerData(player);
+		player.attack(robot);
+
+		if(robot.isDead()){
+			createNewRobot("next");
+		}
+
+	console.log(robot);
+}, intervalMiliSeconds);
+
+	function createNewRobot(position){
+    let id = robot.id;
+    let actualRobotInitialHp = robot.initialHp;
+    let gold = robot.gold;
+		robot = new Robot();
+	  robot.setId(id, position);
+	  robot.setHp(actualRobotInitialHp, position);  
+	  robot.setGold(gold, position);  
+		console.log (position);   
+	}
+  
+  function updateRobotData(robot){
+    var robotName = document.getElementsByClassName('robotNameValue')[0];
+    var robotImage = document.getElementsByClassName('robotImage')[0];
+    var robotHp = document.getElementsByClassName('robotHpValue')[0];
+    var robotGold = document.getElementsByClassName('robotGoldValue')[0];
+
+    robotName.innerHTML = '<h4>Robot #' + robot.id + '</h4>';
+    robotHp.innerHTML = robot.hp;
+    robotGold.innerHTML = robot.gold;
+    robotImage.src='https://robohash.org/' + robot.id;
+  }
+
+  function playerData(player){
+  	var playerName = document.getElementsByClassName('playerNameValue')[0];
+  	var playerAttack = document.getElementsByClassName('AttackValue')[0];
+  	var playerGold = document.getElementsByClassName('GoldValue')[0];
+
+  	playerName.innerHTML = '<h4> ' + player.name + '</h4>';
+  	playerAttack.innerHTML = player.attackDmg;
+  	playerGold.innerHTML = player.gold;
+  	console.log(player)
+
+  }
+
+  function prev(){
+  	createNewRobot("prev");
+  }
+
+  function next(){
+  	createNewRobot("next");
+  }
